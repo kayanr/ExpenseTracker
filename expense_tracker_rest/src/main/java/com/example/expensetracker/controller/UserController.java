15332c0e96c38645738053a7129adabf00750551
Kayanr <kayan.reid@gmail.com>
2020-05-20 20:49:58 -0400

    Adjusted the User Controller and User Repository files.
    Adjusted the User Controller and User Repository files.

--- expense_tracker_rest/src/main/java/com/example/expensetracker/controller/UserController.java
+++ expense_tracker_rest/src/main/java/com/example/expensetracker/controller/UserController.java
@@ -1,4 +1,36 @@
 package com.example.expensetracker.controller;
 
+
+import com.example.expensetracker.model.Expense;
+import com.example.expensetracker.model.User;
+import com.example.expensetracker.repository.ExpenseRepository;
+import com.example.expensetracker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable int id){
+        return userRepository.findById(id);
+    }
+
+    @PostMapping("/user")
+    public User saveUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
 }
--- expense_tracker_rest/src/main/java/com/example/expensetracker/repository/UserRepository.java
+++ expense_tracker_rest/src/main/java/com/example/expensetracker/repository/UserRepository.java
@@ -1,7 +1,14 @@
 package com.example.expensetracker.repository;
 
+import com.example.expensetracker.model.Expense;
 import com.example.expensetracker.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    List<User> findAll();
+    User findById(int id);
+
 }
