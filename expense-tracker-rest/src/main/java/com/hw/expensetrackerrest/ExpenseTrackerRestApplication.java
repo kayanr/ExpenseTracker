9202b964016dd464eef80795c3a6bd53effff773
Kayanr <kayan.reid@gmail.com>
2020-06-09 12:24:25 -0400

    Re-doing the expense-tracker-rest section of app.
    Re-doing the expense-tracker-rest section of app.

---
+++ expense-tracker-rest/src/main/java/com/hw/expensetrackerrest/ExpenseTrackerRestApplication.java
@@ -0,0 +1,13 @@
+package com.hw.expensetrackerrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerRestApplication.class, args);
+	}
+
+}
---
+++ expense-tracker-rest/src/test/java/com/hw/expensetrackerrest/ExpenseTrackerRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hw.expensetrackerrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
